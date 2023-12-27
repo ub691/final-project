10e2e08b3266438600b0a02fb27de5ba70d505c5
ub691 <x9012195407x@gmail.com>
2023-12-27 19:34:33 +0800

    timer

---
+++ source/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+bool Timer(int ms, int id);
+int main()
+{
+	// 時序控制 - 許
+	// 畫面更新 - 陳
+	// 規則設計 - 梁
+}
+bool Timer(int ms, int id)
+{
+	static clock_t t[10];
+	if (clock() - t[id] > ms) {
+		t[id] = clock();
+		return true;
+	}
+	return false;
+}
