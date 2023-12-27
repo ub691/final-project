e314f82d6bd5e413042371eee53f20f175ef256c
cug43 <yoyojan0403@gmail.com>
2023-12-27 19:42:23 +0800

    cursor

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
+#include <Windows.h>
+void HideCursor(void);
+void CursorJump(int x, int y);
 bool Timer(int ms, int id);
+
 int main()
 {
 	// ®É§Ç±±¨î - ³\
@@ -19,4 +22,19 @@ bool Timer(int ms, int id)
 		return true;
 	}
 	return false;
+}
+void HideCursor(void)
+{
+	CONSOLE_CURSOR_INFO curInfo;
+	curInfo.dwSize = 1;
+	curInfo.bVisible = FALSE;
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorInfo(handle, &curInfo);
+}
+
+void CursorJump(int x, int y)
+{
+	COORD pos; pos.X = x; pos.Y = y;
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(handle, pos);
 }
